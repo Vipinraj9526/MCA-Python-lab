0a70d1b27cbc68cf0aac67bf603ac36427eb3e08
Vipinraj9526 <79069656+Vipinraj9526@users.noreply.github.com>
2021-03-24 20:59:28 +0530

    python pgm5 committed

--- CO1/5_over.py
+++ CO1/5_over.py
@@ -1,11 +1,11 @@
-# Prompt the user for a list of integers . For all values greater than 100 ,store over instead
+#5.Prompt the user for a list integers.For all value grater than 100,store'over' instead.
 
-list = []
-limit = int(input("Enter the limit:"))
-for x in range(limit):
-    num = int(input("Enter number " + str(x + 1) + ":"))
-    if (num > 100):
-        num = "over"
-    list.append(num)
-
+list=[]
+a=int(input("Enter the no of values to be inserted:"))
+for x in range(a):
+   n=int(input("Enter the value"))
+   if n<=100:
+      list.append(n)
+   else:
+       list.append('over')
 print(list)
